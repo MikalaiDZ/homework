1061fc21dfb6dad112ebab846f3ee12bd4226a14
mikalayDZ <sekta43@gmail.com>
2022-10-17 17:02:47 +0300

    добавлено меньшее число

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -3,11 +3,6 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2)
-{
-Console.WriteLine($"Большее число из введеных = {num1}");
-}
+if (num1 > num2) Console.WriteLine($"Большее число из введеных = {num1}. Меньшее = {num2}");
 else
-{
-Console.WriteLine($"Большее число из введеных = {num2}");
-}
+ Console.WriteLine($"Большее число из введеных = {num2}. Меньшее = {num1}");
