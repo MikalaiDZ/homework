7947d793fa62f9ab22d68e3e1280e295b2a84879
mikalayDZ <sekta43@gmail.com>
2022-10-17 12:40:00 +0300

    add Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+Console.WriteLine($"Большее число из введеных = {num1}");
+}
+   else
+{
+    Console.WriteLine($"Большее число из введеных = {num2}");
+}
