342c5e47dcd56c9b87635e2cbc912fd09213b55b
mikalayDZ <sekta43@gmail.com>
2022-10-17 16:56:44 +0300

    add Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число N ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 1) 
+    Console.WriteLine("неправильное число. Введите число > 0");
+else
+{
+    int count = 2;
+
+    while (count <= num)
+    {
+        Console.Write($"{count} ");
+        count += 2;
+    }
+}
