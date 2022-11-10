8a539382ff9c841bbd35bc7222b08e6c1a9f517d
mikalaiDZ <sekta43@gmail.com>
2022-11-11 00:41:54 +0300

    Добавлены Task25, Task27, task29

---
+++ Task25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Exponentiation(int num1, int num2)
+{
+    int exp = 1;
+
+    for (int i = 1; i <= num2; i++)
+    {
+        exp *= num1;
+    }
+    return exp;
+}
+
+Console.Write("введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите степень числа: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int exponentiation = Math.Abs(Exponentiation(a, b));
+if (b > 0)
+    if (a < 0) Console.WriteLine($"Число {a} в степени {b} = -{exponentiation}");
+    else Console.WriteLine($"Число {a} в степени {b} = {exponentiation}");
+else Console.WriteLine("Степень числа должна быть натуральной!");
---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int NumberSum(int num)
+{
+    int sum = 0;
+    int last = 0;
+    while (num > 0)
+    {
+        last = num % 10;
+        sum += last;
+        num /=10;
+    }
+    return sum;
+}
+Console.Write("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numbersum = NumberSum(number);
+Console.WriteLine($"Сумма цифр в числе {number} = {numbersum}");
