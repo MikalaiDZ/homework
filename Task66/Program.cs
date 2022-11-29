fcb200a3e8c73236c53daf509e252481c79c8bd0
mikalaiDZ <sekta43@gmail.com>
2022-11-29 17:37:23 +0300

    add Task64, Task66, Task68

---
+++ Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumRecursion(int numM, int numN)
+{
+    if (numM == numN) return numM;
+    return numM += SumRecursion(numM < numN ? numM + 1 : numM - 1, numN);
+}
+Console.Write("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+if (numberM < 0 || numberN < 0)
+{
+    Console.WriteLine("Ошибка! Ведите натуральные числа.");
+}
+else
+{
+    int sumRecursion = SumRecursion(numberM, numberN);
+    Console.WriteLine($"Сумма натуральных элементов от {numberM} до {numberN} = {sumRecursion}");
+}
---
+++ Task68/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+uint AckermanFunction(uint numM, uint numN)
+{
+    if (numM == 0) return numN + 1;
+    if ((numM != 0) && (numN == 0)) return AckermanFunction(numM - 1, 1);
+    return AckermanFunction(numM - 1, AckermanFunction(numM, numN - 1));
+}
+
+Console.Write("Введите число M: ");
+uint numberM = Convert.ToUInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+uint numberN = Convert.ToUInt32(Console.ReadLine());
+
+if (numberM < 0 || numberN < 0)
+{
+    Console.WriteLine("Ошибка! Введите натуральные числа.");
+}
+else
+{
+    uint ackermanFunction = AckermanFunction(numberM, numberN);
+    Console.Write($"Результат = {ackermanFunction}");
+}
